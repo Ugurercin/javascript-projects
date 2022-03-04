dde77ff135dfc28b110184928af765ccd2fd9e45
Ugur <ercinugur@gmail.com>
2022-03-04 20:29:43 +0300

    localstorage added

--- 009_ExpenseTracker/script.js
+++ 009_ExpenseTracker/script.js
@@ -7,14 +7,10 @@ const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
 
-const dummyTransactions = [
-  { id: 1, text: 'Flower', amount: -20},
-  { id: 2, text: 'Salary', amount: 300},
-  { id: 3, text: 'Book', amount: -10},
-  { id: 4, text: 'Camera', amount: 150}
-];
+const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'))
 
-let transactions = dummyTransactions;
+
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
 
 //add transaction
 
@@ -33,6 +29,8 @@ function addTransaction(e) {
     transactions.push(transaction);
     addTransactionDOM(transaction);
     updateValues();
+
+    updateLocalStorage();
     text.value = '';
     amount.value = '';
   }
@@ -82,7 +80,13 @@ function updateValues() {
 function removeTransaction(id) {
   transactions = transactions.filter(transaction => transaction.id !== id)
 
+  updateLocalStorage();
+
   init();
+}
+
+function updateLocalStorage() {
+  localStorage.setItem('transactions', JSON.stringify(transactions))
 }
 
 //Init app
