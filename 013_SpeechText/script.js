eabe3c231a2f075bd4aad1c14e5f68880cb0ffdd
Ugur <ercinugur@gmail.com>
2022-03-11 14:03:59 +0300

    final version

--- 013_SpeechText/script.js
+++ 013_SpeechText/script.js
@@ -1,4 +1,4 @@
-const main = document.querySelector('main');
+const main = document.getElementById('main')
 const voicesSelect = document.getElementById('voices');
 const textArea = document.getElementById('text');
 const readBtn = document.getElementById('read');
@@ -69,5 +69,56 @@ function createBox(item) {
     <p class="info">${text}</p>
   `;
 
+  box.addEventListener('click', () => {
+    setTextMessage(text);
+    speakText();
+
+    box.classList.add('active');
+    setTimeout(() => box.classList.remove('active'), 800);
+  })
+
   main.appendChild(box);
 }
+
+const message = new SpeechSynthesisUtterance();
+
+let voices = [];
+
+function getVoices() {
+  voices = speechSynthesis.getVoices();
+
+  voices.forEach(voice => {
+    const option = document.createElement('option');
+
+    option.value = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`;
+
+    voicesSelect.appendChild(option);
+  });
+}
+
+function setVoice(e) {
+  message.voice = voice.find(voice => voice.name === e.target.value)
+}
+
+function setTextMessage(text) {
+  message.text = text;
+}
+
+function speakText() {
+  speechSynthesis.speak(message);
+}
+
+speechSynthesis.addEventListener('voiceschanged', getVoices);
+
+toggleBtn.addEventListener('click', () => document.getElementById('text-box').classList.toggle('show'))
+closeBtn.addEventListener('click', () => document.getElementById('text-box').classList.remove('show'))
+
+voicesSelect.addEventListener('change', setVoice)
+
+readBtn.addEventListener('click', () => {
+  setTextMessage(textArea.value);
+  speakText();
+})
+
+getVoices();
